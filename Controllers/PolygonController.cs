32b38c0e4e25240a65d87f4cbbf8defcf442c560
Тимофей Данилюк <timofei.danilyuk.kuzstu@gmail.com>
2024-08-02 16:37:41 +0700

    Добавьте файлы проекта.

---
+++ Controllers/PolygonController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_B2G.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+
+
+namespace Test_B2G.Controllers
+{
+    public class PolygonController : Controller
+    {
+        private static List<Models.Point> points = new List<Models.Point>();
+        private static bool polygonDrawn = false;
+        private static Models.Point[] polygonPoints;
+
+        public IActionResult Index()
+        {
+            ViewBag.Points = points;
+            ViewBag.PolygonDrawn = polygonDrawn;
+            ViewBag.PolygonPoints = polygonPoints;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPoint([FromBody] Models.Point point)
+        {
+            if (polygonDrawn)
+            {
+                bool isInside = PolygonHelper.IsPointInPolygon(point, points);
+                string message = isInside ? "Эта точка находится внутри полигона" : "Эта точка находится вне полигона";
+                return Json(new { message });
+            }
+            else
+            {
+                points.Add(point);
+                return Json(new { });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DrawPolygon()
+        {
+            if (points.Count > 2)
+            {
+                polygonPoints = points.ToArray();
+                polygonDrawn = true;
+                return Json(new { });
+            }
+            else
+            {
+                return Json(new { message = "Поставьте три точки чтобы нарисовать полигон." });
+            }
+        }
+        [HttpPost]
+        public IActionResult ClearPolygon()
+        {
+            points.Clear();
+            polygonDrawn = false;
+            polygonPoints = null;
+            return Json(new { message = "Полигон очищен" });
+        }
+    }
+}
---
+++ Models/PolygonHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+namespace Test_B2G.Models
+{
+    public class PolygonHelper
+    {
+        public static bool IsPointInPolygon(Point point, List<Point> polygon)
+        {
+            int polygonLength = polygon.Count;
+            bool inside = false;
+
+            Point endPoint = polygon[polygonLength - 1];
+
+            foreach (var startPoint in polygon)
+            {
+                if ((startPoint.Y <= point.Y && point.Y < endPoint.Y || endPoint.Y <= point.Y && point.Y < startPoint.Y) &&
+                    (point.X < (endPoint.X - startPoint.X) * (point.Y - startPoint.Y) / (endPoint.Y - startPoint.Y) + startPoint.X))
+                {
+                    inside = !inside;
+                }
+                endPoint = startPoint;
+            }
+            return inside;
+        }
+    }
+}
